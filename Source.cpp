95567d1cb4a6f02fa75fa9eecbdfdf06fd4ca005
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-27 20:57:56 +0500

    Code in progress

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,122 @@
 
 using namespace std; 
 
+class node {
+public:
+	Product data;
+	node* next;
+	node() {
+		data = Product(0, 0, "", "", 0.0);
+		next = NULL;
+	}
+};
+
+class LinkedList {
+	node* head;
+public:
+	LinkedList() {
+		head = NULL;
+	}
+	node* create_New_Node(Product data) {
+		node* newNode = new node;
+		newNode->data = data;
+		return newNode;
+	}
+	int insertItem(Product data) {
+		node* newNode = create_New_Node(data);
+		if (head == NULL) {
+			head = newNode;
+		}
+		else {
+			node* temp_node = head;
+			while (temp_node->next != NULL) {
+				temp_node = temp_node->next;
+			}
+			temp_node->next = newNode;
+		}
+		return 1;
+	}
+	int delete_Item(int id, string name) {
+		if (head == NULL) {
+			cout << "Link list is empty there is nothing to delete in it.";
+
+		}
+		else {
+			node* temp_node = head;
+			// Tranversing pointer node->next-next to easily delete the desired item and to connect the prev and next node easily.
+			while (temp_node->next->next != NULL) {
+				if (temp_node->next->data.id == id && temp_node->next->data.name == name) {
+					break;
+				}
+				temp_node = temp_node->next;
+			}
+			node* node_to_del = temp_node;
+			temp_node = temp_node->next->next;
+			delete node_to_del;
+
+		}
+		return 1;
+	}
+
+	int update_item(int id, string name) {
+		if (head == NULL) {
+			cout << "Link list is empty there is nothing to delete in it.";
+
+		}
+		else {
+			node* temp_node = head;
+			// Tranversing pointer node->next-next to easily delete the desired item and to connect the prev and next node easily.
+			while (temp_node->next->next != NULL) {
+				if (temp_node->next->data.id == id && temp_node->next->data.name == name) {
+					break;
+				}
+				temp_node = temp_node->next;
+			}
+			cout << "Enter 1 if you want to update the id of the product." << endl;
+			cout << "Enter 2 if you want to update the name of the product. " << endl;
+			cout << "Enter 3 if you want to update Company name of the product. " << endl;
+			cout << "Enter 4 if you want to update the quantity of the product. " << endl;
+			cout << "Enter 5 if you want to update the price of the product. " << endl;
+			int choice = 0;
+			cout << "Enter your choice here : ";
+			cin >> choice;
+			switch (choice)
+			{
+			case 1:
+				cout << "Enter updated id of the product : ";
+				int id;
+				cin >> id;
+				temp_node->data.name = name;
+				break;
+			case 2:
+				temp_node->data.name = "";
+				cout << "Enter updated name of the product : ";
+				cin >> temp_node->data.name;
+				break;
+			case 3:
+				temp_node->data.company_name = "";
+				cout << "Enter the updated Company Name of product : ";
+				cin >> temp_node->data.company_name;
+				break;
+			case 4:
+				cout << "Enter the updated quantity of Product : ";
+				temp_node->data.quantity = 0;
+				cin >> temp_node->data.quantity;
+				break;
+			case 5:
+				cout << "Enter the updated price of Product : ";
+				cin >> temp_node->data.price;
+			default:
+				break;
+			}
+
+		}
+	}
+
+
+
+};
+
 class Identity {
 public:
 	long int id;
@@ -21,7 +137,7 @@ public:
 	}
 };
 
-class Customer : protected Identity {
+class Customer : public Identity {
 public:
 	long int phone_number;
 	string address;
@@ -33,6 +149,9 @@ public:
 		this->phone_number = phone_number;
 		this->address = address;
 	}
+	void display() {
+
+	}
 
 };
 
@@ -40,18 +159,38 @@ class Product : public Identity {
 public:
 	string company_name; 
 	int quantity;
+	long float price;
 	Product() {
 		company_name = "";
 		quantity = 0;
+		price = 0.0;
 	}
 	Product(int id, string name, string company_name):Identity(id, name) {
 		this->company_name;
 	}
-	Product(int id, int quantity,  string name, string company_name):Identity(id, name) {
+	Product(int id, int quantity,  string name, string company_name, long float price):Identity(id, name) {
 		this->company_name = company_name;
 		this->quantity = quantity;
+		this->price = price;
 	}
 };
+class Inventory {
+public:
+	list<Product> l1;
+	int add_item(int id, string name, int quantity, string company_name, long float price) {
+
+		Product p;
+		p = Product(id, quantity, name, company_name, price);
+		l1.push_back(p);
+	}
+	int del_item(string name) {
+		for (int i = 0; i < l1.size(); i++) {
+			Product temp;
+			l1.front();
+		}
+	}
+};
+
 
 class Order: protected Product {
 public:
@@ -72,30 +211,13 @@ public:
 			string item = "";
 			getline(cin, item);
 			this->order_item[i] = item;
+			
 
 		}
 	}
 };
 
 
-class Inventory {
-public:
-	list<Product> l1;
-	Product p;
-	int add_item(int id, string name, int quantity, string company_name) {
-		p.id = id;
-		p.quantity = quantity;
-		p.name = name;
-		p.company_name = company_name;
-		l1.push_back(p);
-	}
-	int del_item(string name, int quantity) {
-		for (int i = 0; i < l1.size(); i++) {
-			;
-		}
-	}
-
-};
 
 class Functionality {
 public:
@@ -105,5 +227,8 @@ public:
 };
 
 int main() {
-	return 0;
+	Identity* id;
+	Customer c;
+	id = &c;
+	return 1;
 }
